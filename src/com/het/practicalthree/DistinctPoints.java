05697347efb73ca0c285b54de3ed93db530d4dde
HETPATEL2008 <hetp5478@gmail.com>
2026-07-14 19:30:09 +0530

    feat: Add Problem-01 of Practical-03

---
+++ src/com/het/practicalthree/DistinctPoints.java
@@ -0,0 +1,65 @@
+package com.het.practicalthree;
+
+import java.util.Objects;
+
+class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
+
+class Driver {
+
+    public static void main(String[] args) {
+
+        Point[] points = {
+                new Point(1, 2),
+                new Point(3, 4),
+                new Point(1, 2),
+                new Point(5, 6),
+                new Point(3, 4)
+        };
+
+        int distinct = 0;
+
+        for (int i = 0; i < points.length; i++) {
+
+            boolean found = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (points[i].equals(points[j])) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                distinct++;
+        }
+
+        System.out.println("Distinct: " + distinct);
+    }
+}
